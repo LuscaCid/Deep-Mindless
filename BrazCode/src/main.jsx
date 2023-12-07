45f641d68ac0aed572af63f85672d280ed0bd81b
Lucas Cid <lucasfelipaaa@gmail.com>
2023-12-07 14:42:22 +0000

    project altered

--- BrazCode/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {PrincipalPage} from './principal/principalPage'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PrincipalPage />
-  </React.StrictMode>,
-)
